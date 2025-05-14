1abf1b2a8b0e368f80e037602b87776981cca5fa
Jalpachavda-hb <jalpachavdahackbarry@gmail.com>
2025-05-14 15:09:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename="/box_file">
+  <HashRouter>
     <App />
   </HashRouter>
 );
