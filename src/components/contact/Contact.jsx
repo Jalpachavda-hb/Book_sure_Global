c2fa82980e60d8800cc469ed4ab30a792ef92d63
jalpachavda <japuchavda2002@gmail.com>
2025-12-31 15:34:54 +0530

    slove vercel err

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+
+// import PageBanner from "../PageBanner/PageBanner";
+// import bannerImg from "../../assets/Images/Aboutus/breadcrumb_bg.jpg";
+// import Faq from "./Faq";
+// import ExpertsSection from "./Expertsection";
+// import GetInTouch from "./Getintouch";
+// const Contact = () => {
+//   return (
+//     <>
+//       <PageBanner title="Contact Us" bgImage={bannerImg} />
+//       <ExpertsSection/>
+//       <GetInTouch/>
+//       <Faq/>
+//     </>
+//   );
+// };
+
+// export default Contact;
+
+
+import React from 'react'
 import PageBanner from "../PageBanner/PageBanner";
 import bannerImg from "../../assets/Images/Aboutus/breadcrumb_bg.jpg";
-import Faq from "./Faq";
-import ExpertsSection from "./Expertsection";
-import GetInTouch from "./Getintouch";
+import Expertsection from "../Contact/Expertsection";
+import GetInTouch from "../Contact/Getintouch"
+import Faq from "../Contact/Faq";
 const Contact = () => {
   return (
-    <>
-      <PageBanner title="Contact Us" bgImage={bannerImg} />
-      <ExpertsSection/>
-      <GetInTouch/>
-      <Faq/>
-    </>
-  );
-};
+   <>
+    <PageBanner title="Contact Us" bgImage={bannerImg} />
+    <Expertsection/>
+    <GetInTouch/>
+    <Faq/>
+   </>
+  )
+}
 
-export default Contact;
+export default Contact
